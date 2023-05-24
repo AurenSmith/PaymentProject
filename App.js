1c6e8cf6cb18ea01a9a8987ec4a31e73cf20911a
Auren Smith <270174859@yoobeestudent.ac.nz>
2023-05-24 18:13:12 +1200

    React Native Project Add

--- App.js
+++ App.js
@@ -1,20 +1,139 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, PanResponder, Animated } from 'react-native';
+import { useState, useRef } from 'react';
+import { FontAwesome } from '@expo/vector-icons';
+import { styles } from "./Style";
+
+const MyButton = ({ onPress }) => {
+  return (
+    <TouchableOpacity style={styles.sidebarButton} onPress={onPress}>
+      {/* Add content for the button */}
+    </TouchableOpacity>
+  );
+};
+
+const LiveChatButton = ({ onPress }) => {
+  return (
+    <TouchableOpacity style={styles.liveButton} onPress={onPress}>
+      {/* Add content for the liveButton */}
+    </TouchableOpacity>
+  );
+};
 
 export default function App() {
+  const [showAdditionalObject, setShowAdditionalObject] = useState(false);
+  const [showLiveChat, setShowLiveChat] = useState(false);
+  const sidebarAnimation = useRef(new Animated.Value(0)).current;
+  const liveChatAnimation = useRef(new Animated.Value(0)).current;
+
+  const handleSidebarButtonPress = () => {
+    setShowAdditionalObject(true);
+    Animated.timing(sidebarAnimation, {
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  const handleSidebarClose = () => {
+    Animated.timing(sidebarAnimation, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: false,
+    }).start(() => {
+      setShowAdditionalObject(false);
+    });
+  };
+
+  const handleLiveChatButtonPress = () => {
+    setShowLiveChat(true);
+    Animated.timing(liveChatAnimation, {
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  const handleLiveChatClose = () => {
+    Animated.timing(liveChatAnimation, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: false,
+    }).start(() => {
+      setShowLiveChat(false);
+    });
+  };
+
+  const sidebarTranslateX = sidebarAnimation.interpolate({
+    inputRange: [0, 1],
+    outputRange: [-300, 0], // Adjust the value based on your sidebar width
+  });
+
+  const liveChatHeight = liveChatAnimation.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, 400], // Adjust the value based on your liveChat expanded height
+  });
+
+  const panResponder = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: (_, { dx }) => dx < -10, // Swipe left threshold
+      onPanResponderMove: (_, { dx }) => {
+        // Handle swipe left to close sidebar
+        if (dx < -10) {
+          handleSidebarClose();
+        }
+      },
+    })
+  ).current;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <MyButton onPress={handleSidebarButtonPress} />
+
+      {showAdditionalObject && (
+        <Animated.View
+          style={[
+            styles.sidebar,
+            {
+              transform: [{ translateX: sidebarTranslateX }],
+            },
+          ]}
+          {...panResponder.panHandlers}
+        >
+          {/* Add content for the sidebar */}
+        </Animated.View>
+      )}
+
+      <LiveChatButton onPress={handleLiveChatButtonPress} />
+
+      {showLiveChat && (
+        <Animated.View
+          style={[
+            styles.liveChat,
+            {
+              height: liveChatHeight,
+            },
+          ]}
+        >
+          <TouchableOpacity onPress={handleLiveChatClose} style={styles.liveChatCloseButton}>
+            <FontAwesome name="times-circle-o" size={28} style={styles.liveChatCloseButtonText} />
+          </TouchableOpacity>
+          <View style={styles.liveTopbar}></View>
+
+          <View style={styles.liveContent}>
+            <Text style={styles.liveChatText}>Live Chat Content</Text>
+          </View>
+
+          <View style={styles.liveBottombar}>
+            <Text style={styles.liveMessage}>Message</Text>
+            <View style={styles.sendContainer}>
+              <FontAwesome name="paper-plane" size={20} style={styles.liveSend} />
+            </View>
+          </View>
+        </Animated.View>
+      )}
+
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Style.js
@@ -0,0 +1,129 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  sidebarButton: {
+    position: 'absolute',
+    top: 39,
+    left: 23,
+    width: 61,
+    height: 61,
+    backgroundColor: '#737373',
+    borderRadius: 20,
+  },
+  sidebar: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '80%',
+    height: '100%',
+    borderTopRightRadius: 20,
+    borderBottomRightRadius: 20,
+    backgroundColor: '#737373',
+  },
+  liveButton: {
+    position: 'absolute',
+    top: 39,
+    right: 23,
+    width: 61,
+    height: 61,
+    backgroundColor: '#737373',
+    borderRadius: 20,
+  },
+  liveChat: {
+    position: 'absolute',
+    top: 39,
+    right: 23,
+    left: 23,
+    height: 400,
+    borderRadius: 20,
+    backgroundColor: '#797979',
+  },
+  liveContent: {
+    position: 'absolute',
+    top: 61,
+    left: 0,
+    right: 0,
+    height: 280,
+  },
+  liveChatText: {
+    position: 'relative',
+    color: '#fff',
+    fontSize: 20,
+    marginBottom: 20,
+  },
+  liveChatCloseButton: {
+    position: 'absolute',
+    right: 0,
+    top: 0,
+
+    backgroundColor: '#737373',
+    borderTopRightRadius: 20,
+    width: 61,
+    height: 61,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  liveChatCloseButtonText: {
+    color: '#FFFFFF',
+    fontWeight: 400,
+    position: 'absolute',
+    width: 28,
+    height: 28,
+  },
+  liveTopbar: {
+    position: 'absolute',
+    top: 0,
+    right: 61,
+    left: 0,
+    height: 61,
+    borderTopLeftRadius: 20,
+
+    backgroundColor: '#737373',
+  },
+  liveBottombar: {
+    position: 'absolute',
+    bottom: 0,
+    height: 61,
+    left: 0,
+    right: 0,
+
+    backgroundColor: '#737373',
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+  },
+  liveMessage: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 61,
+    height: 61,
+
+    fontSize: 20,
+    color: '#ffffff',
+    marginLeft: 10,
+    marginTop: 4,
+  },
+  sendContainer: {
+    position: 'absolute',
+    right: 0,
+    bottom: 0,
+    height: 61,
+    width: 40,
+
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  liveSend: {
+    textAlign: 'center',
+    color: '#ffffff',
+  },
+});
+
+export { styles };
