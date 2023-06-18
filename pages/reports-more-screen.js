b2240ac5a0045f1dfeba86c1c8170064628589b4
Infateshands <90697583+Infateshands@users.noreply.github.com>
2023-06-19 11:01:05 +1200

    Updated reports-more screen
    working list, click order to open order component.

--- pages/reports-more-screen.js
+++ pages/reports-more-screen.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView, FlatList } from 'react-native';
 import { useState, useRef } from 'react';
 import { FontAwesome } from '@expo/vector-icons';
 
@@ -8,10 +8,13 @@ import { FontAwesome } from '@expo/vector-icons';
 import { styles } from "../Style";
 import Sidebar from '../components/Sidebar';
 
+
 export default function HomeScreen() {
   const [showLiveChat, setShowLiveChat] = useState(false);
   const [showSidebar, setShowSidebar] = useState(false);
+  const [showOrder, setShowOrder] = useState(false);
 
+  
   const sidebarOpen = () => {
     setShowSidebar(true);
   };
@@ -26,6 +29,13 @@ export default function HomeScreen() {
 
   const handleLiveChatClose = () => {
     setShowLiveChat(false);
+  };
+  const handleOrderPress = () => {
+    setShowOrder(true);
+  }
+
+  const handleOrderClose = () => {
+    setShowOrder(false);
   };
 
   return (
@@ -43,17 +53,82 @@ export default function HomeScreen() {
         
       </View>
       <View style={local.orders}>
-        
+        <FlatList
+        // temp data
+          data={[
+            {key: ' 1 Jan                    Recipient1                 -$0.00 '},
+            {key: ' 1 Jan                    Recipient2                 -$0.00 '},
+            {key: ' 1 Jan                    Recipient3                 -$0.00 '},
+            {key: ' 1 Jan                    Recipient4                 -$0.00 '},
+            {key: ' 1 Jan                    Recipient5                 -$0.00 '},
+            {key: ' 1 Jan                    Recipient6                 -$0.00 '},
+            {key: ' 1 Jan                    Recipient7                 -$0.00 '},
+            {key: ' 1 Jan                    Recipient8                 -$0.00 '},
+            {key: ' 1 Jan                    Recipient9                 -$0.00 '},
+            
+          ]}
+          renderItem={({item}) => 
+          <TouchableOpacity style={local.order} onPress={handleOrderPress}>
+            <Text style={local.item}>{item.key}</Text>
+          </TouchableOpacity>}
+          />
       </View>
+      <TouchableOpacity style={local.button}>
+        <Text style={local.buttonText}>Other</Text>
+      </TouchableOpacity>
+      <View style={{flexDirection: 'row', marginTop: 20, paddingBottom: 30}}>
+          <TouchableOpacity style={local.buttonSquare}>
+            
+
+            
+
+          </TouchableOpacity>
+          <TouchableOpacity style={local.button}>
+            <Text style={local.buttonText}>Generate Purchase</Text>
+          </TouchableOpacity>
+          
+        </View>
       
 
       
+      {/* order asset */}
+          
+      {showOrder && (
+        <View 
+        style={local.orderContainer}>
 
-      
+          <Text style={{fontSize: 16}}>Recipient</Text>
+          <Text style={{fontSize: 20, marginBottom: 10}}>$-0.00</Text>
+          <Text style={{fontSize: 14, marginBottom: 10}}>12:00pm, Mon 31 January 2023 </Text>
+          <Text style={{fontSize: 12, fontWeight: 'bold'}}>Details </Text>
+          <Text style={{fontSize: 12, marginBottom: 10}}>Card Number: 1234 **** **** 1234</Text>
+          <Text style={{fontSize: 12}}>Notes</Text>
+          <View style={local.notes} />
 
-      {showSidebar && (
+          
+
+          
+          {/*close button */}
+          <TouchableOpacity 
+          style={local.close}
+          onPress={handleOrderClose}>
+            <Text style={{color: 'white', fontWeight: 'bold'}}>Close</Text>
+          </TouchableOpacity>
+          
+
+          
+        
+
+        </View>
+      )}
+
+      {showSidebar &&(
         <Sidebar onClose={sidebarClose}/>
       )}
+
+      
+
+      
 
       {/* live chat asset */}
       {showLiveChat && (
@@ -96,9 +171,80 @@ export default function HomeScreen() {
 const local = StyleSheet.create({
     orders: {
         marginTop: 50,
-        
+        paddingTop: 5,
+        padding: 10,
+        borderRadius: 15,
         width: '80%',
         backgroundColor: 'grey',
+        marginBottom: 200,
         
-    }
+    },
+    order: {
+      backgroundColor: '#737373',
+      borderRadius: 10,
+      padding: 1,
+      marginTop: 10
+    },
+
+    item: {
+      color: 'white',
+      fontWeight: 'bold'
+    },
+    button: {
+      width: 200,
+      height: 40,
+      backgroundColor: '#258699',
+      padding: 10,
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: 30,
+      
+    },
+    buttonText: {
+      color: 'white',
+      fontWeight: 'bold',
+      fontSize: 16
+    },
+    buttonSquare: {
+      backgroundColor: 'black',
+      borderRadius: 15,
+      width: 40,
+      height: 40,
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginRight: 15
+    },
+    orderContainer: {
+      height: '35%',
+      width: '80%',
+      borderRadius: 20,
+      backgroundColor: '#666666',
+      position: 'absolute',
+      top: '25%',
+      left: '10%',
+      padding: 10
+  },
+    close: {
+     width: '50%',
+     height: 30,
+     backgroundColor: '#737373',
+     marginLeft: '25%',
+     marginTop: 10,
+     borderRadius: 20,
+     alignItems: 'center',
+     justifyContent: 'center'
+  },
+  orderConent: {
+   postion: 'absolute',
+   left: 10,
+   top: 10
+  },
+  notes: {
+    width: '80%',
+    height: '30%',
+    backgroundColor: '#737373',
+    borderRadius: 15,
+    marginLeft: '10%'
+  }
+
 })
